e2c8c37f07d81407a209c6e75af8e336da6127f9
wilderjose <wilder.suarez@est.ulsa.edu.ni>
2026-04-09 16:36:16 -0600

    primera version

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,10 +7,12 @@ import './App.css'
 function App() {
   return (
     <div className="flex h-screen w-full items-center justify-center bg-gray-100">
-  <p className="bg-blue-700 text-white p-6 text-center rounded-xl shadow-lg">
-    Hello Vite + React!
-  </p>
-</div>
+      <p className="bg-blue-700 text-white p-6 text-center rounded-xl shadow-lg">
+        Hello Vite + React!
+      </p>
+
+      <h1><Hola>Mi Primera App en React</Hola></h1>
+    </div>
   )
 }
 export default App
