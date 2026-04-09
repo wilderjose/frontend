439af837dbcc16f076768772eb031020c631ecce
wilderjose <wilder.suarez@est.ulsa.edu.ni>
2026-04-09 16:29:15 -0600

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-gray-100">
+  <p className="bg-blue-700 text-white p-6 text-center rounded-xl shadow-lg">
+    Hello Vite + React!
+  </p>
+</div>
+  )
+}
+export default App
